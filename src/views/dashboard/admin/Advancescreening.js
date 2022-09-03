b3a63bab8a24581d6cebb7daae94cbdbdd8af9fb
devsumagoinfotech <dev@sumagoinfotech.com>
2022-09-03 10:15:48 +0530

    remove match field

--- src/views/dashboard/admin/Advancescreening.js
+++ src/views/dashboard/admin/Advancescreening.js
@@ -112,7 +112,7 @@ componentDidMount() {console.log("DID MOUNT ************");
 		this.mounted = true;
 		//this.setState({data:null});
         
-		 axios.post("http://159.65.148.197:3001/api/generalsurvey/screeningScreener",{issubscreener:0})
+		 axios.post("http://159.65.148.197:3001/api/generalsurvey/screeningScreener")
 		 .then(response => {
 					console.log("Returned data:", response.data.status);
 					if(response.data.status===1)
