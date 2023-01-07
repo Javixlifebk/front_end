2d12a33bec6e3b0a911fef3e6dc3861a7e642b19
devsumagoinfotech <dev@sumagoinfotech.com>
2023-01-07 20:17:35 +0530

    modify prescription report

--- src/views/dashboard/doctor/MedicineList.js
+++ src/views/dashboard/doctor/MedicineList.js
@@ -98,7 +98,7 @@ loadRecs(recs)
  handleClick(_caseId)  {    
 
 
-  axios.post('http://javixlife.org:3010/api/report/createPrescriptionReport?=', { caseId:_caseId})
+  axios.post('http://javixlife.org:3010/api/report/createPrescriptionReport?=', { caseId:_caseId,ngoId:localStorage.getItem("ngoId")})
   .then(response => {
        if(response.data.status===1)
          {
